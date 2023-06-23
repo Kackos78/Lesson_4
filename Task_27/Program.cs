7b5b6113233e95cccd984ed41fa046567c3ae46f
Daniil <oleinik.d_78@mail.ru>
2023-06-23 18:37:11 +0400

    Task_29 done

---
+++ Task_27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+System.Console.WriteLine("Введите число: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+string numInStr = Console.ReadLine();
+int ArrLenght = numInStr.Length;
+int DoSumNum(string numInStr)
+{
+    int ArrLenght = numInStr.Length;
+    string[] arrNumStr = { };
+    for (int i = 0; i < ArrLenght; i++)
+    {
+        arrNumStr[i] = numInStr[i];
+        
+    }
+    return 
+}
---
+++ Task_29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] arr = new int[8];
+Random rnd = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(0, 10);
+// System.Console.Write($"{arr[i]} ");
+}
+
+// for (int i = 0; i < arr.Length - 1; i++)
+// {
+// System.Console.Write($"{arr[i]}; ");
+// }
+// System.Console.Write($"{arr[arr.Length - 1]}");
+
+// System.Console.WriteLine();
+System.Console.WriteLine($"[{string.Join("; ", arr)}]");
